5f05aa4fae41acad874612a1bc427ad38fb0969b
zhangrui10 <zhangrui10@yy.com>
2021-04-24 02:10:36 +0800

    feat: temp2

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import "./styles.css";
 import {
   Layout,
@@ -15,19 +16,28 @@ import {
   FileOutlined,
   FormOutlined,
 } from "@ant-design/icons";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { SpaceContext } from "antd/lib/space";
 import { connOption } from "./config";
 import Peer from "peerjs";
+import CanvasDraw from "react-canvas-draw";
 const { Header, Content, Footer } = Layout;
 
 const { Title, Text } = Typography;
 
 const key = "key" + Math.floor(Math.random() * 1000000);
 let myPeer: any;
+let globalConn: any;
+let lockTimer;
+let lock = "";
+let localLock = 0;
+// @ts-ignore
+
 export default function App() {
   const [isConnecting, setIsConnecting] = useState(false);
   const [inputValue, setInputValue] = useState<string>();
+  const [whiteBoard, setWhiteBoard] = useState(false);
+  const canvas = useRef(null);
 
   const handleCall = useCallback(
     (call?: any) => {
@@ -40,20 +50,24 @@ export default function App() {
       navigator.getUserMedia(
         mediaObj,
         function (stream) {
-          const videoLocal: any = document.getElementById("video1");
-          if (videoLocal) {
-            videoLocal.srcObject = stream;
-            videoLocal.play();
-          }
-          if (!call) call = myPeer.call(inputValue, stream);
-          else call.answer(stream);
-          call.on("stream", (stream: any) => {
-            const videoRemote: any = document.getElementById("video2");
-            if (videoRemote) {
-              videoRemote.srcObject = stream;
-              videoRemote.play();
+          try {
+            const videoLocal: any = document.getElementById("video1");
+            if (videoLocal) {
+              videoLocal.srcObject = stream;
+              videoLocal.play();
             }
-          });
+            if (!call) call = myPeer.call(inputValue, stream);
+            else call.answer(stream);
+            call.on("stream", (stream: any) => {
+              const videoRemote: any = document.getElementById("video2");
+              if (videoRemote) {
+                videoRemote.srcObject = stream;
+                videoRemote.play();
+              }
+            });
+          } catch {
+            console.error("video play error");
+          }
         },
         () => console.log("error")
       );
@@ -84,7 +98,18 @@ export default function App() {
         handleCall();
       } else if (data.type === "refuse") {
         message.error("对方已拒绝邀请");
+        setInputValue(undefined);
         setTimeout(() => conn.close(), 1000);
+      } else if (data.type === "close") {
+        message.error("对方已挂断");
+        setInputValue(undefined);
+        setTimeout(() => conn.close(), 1000);
+      } else if (data.type === "white") {
+        setWhiteBoard(data.payload);
+      } else if (data.type === "draw") {
+        canvas.current.loadSaveData(data.payload.data);
+      } else if (data.type === "lock") {
+        lock = data.payload;
       }
     } catch {
       console.log("所接受数据格式错误");
@@ -99,22 +124,27 @@ export default function App() {
         message.error("初始化连接失败");
       });
       myPeer.on("open", () => {
-        message.success("自动注册成功");
+        console.log("自动注册成功");
       });
       myPeer.on("call", (call: any) => {
         handleCall(call);
       });
       // @ts-ignore
       myPeer.on("connection", (conn) => {
+        globalConn = conn;
         // @ts-ignore
         conn.on("data", handleReceive(conn));
       });
       myPeer.on("close", () => {
-        isConnecting && setIsConnecting(false);
         message.error("通话已挂断");
       });
+      // myPeer.on("disconnected", () => {
+      //   isConnecting && setIsConnecting(false);
+      //   setInputValue(undefined);
+      //   message.error("通话已挂断");
+      // });
     }
-  }, [isConnecting]);
+  }, []);
   useEffect(() => {
     let conn: any;
     if (inputValue) {
@@ -131,182 +161,254 @@ export default function App() {
           },
         });
       });
+      globalConn = conn;
       // @ts-ignore
       conn.on("data", handleReceive(conn));
     }
     return () => {
-      if (conn) conn.close();
+      console.log("inputValue hook clean", conn);
+      if (conn) {
+        conn.send({ type: "close" });
+        setTimeout(() => {
+          conn.close();
+        }, 1000);
+      }
     };
   }, [inputValue]);
+  useEffect(() => {
+    if (!inputValue) {
+      const videoLocal: any = document.getElementById("video1");
+      const videoRemote: any = document.getElementById("video2");
+      videoLocal.pause();
+      videoRemote.pause();
+      isConnecting && setIsConnecting(false);
+    }
+  }, [inputValue]);
   return (
-    <Layout className="layout" style={{ minHeight: "100vh" }}>
-      <Header>
-        <div style={{ color: "white", fontSize: "20px" }}>
-          <Space size="middle">
-            <VideoCameraTwoTone />
-            在线授课系统
-            <Button
-              onClick={() => document.documentElement.requestFullscreen()}
-            >
-              全屏
-            </Button>
-          </Space>
-        </div>
-      </Header>
+    <>
       <div
-        className="panel"
         style={{
-          display: !isConnecting ? "flex" : "none",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "80vh",
+          zIndex: 10,
+          background: "rgba(255, 255, 255, 0.5)",
+          display: whiteBoard ? "" : "none",
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
         }}
       >
-        <Card style={{ width: "500px", height: "300px", margin: "32px" }}>
-          <Space direction="vertical">
-            <Title level={4}>基本信息</Title>
-            <div style={{ fontSize: "16px" }}>
-              个人ID：<Text type="secondary">{key}</Text>
-            </div>
-            <div style={{ fontSize: "16px" }}>
-              在线时长：<Text type="secondary">{"<10分钟"}</Text>
-            </div>
-          </Space>
-        </Card>
-        <Card style={{ width: "500px", height: "300px" }}>
-          <Space direction="vertical" style={{ width: "100%" }}>
-            <Title level={4}>发起聊天</Title>
-            <div>
-              <Input.Search
-                onSearch={(value) => setInputValue(value)}
-                placeholder="请输入对方的ID"
-              />
-            </div>
-          </Space>
-        </Card>
+        <CanvasDraw
+          ref={(value) => (canvas.current = value)}
+          lazyRadius={5}
+          brushRadius={2}
+          hideGrid
+          style={{ width: "100vw", height: "100vh", background: "none" }}
+          disabled={lock !== "" && lock !== key}
+          onChange={(value) => {
+            console.log(canvas.current.getSaveData());
+            if (lock === "" || lock === key) {
+              if (lock === "") {
+                lock = key;
+                globalConn.send({
+                  type: "lock",
+                  payload: key,
+                });
+              }
+              clearTimeout(lockTimer);
+              lockTimer = setTimeout(() => {
+                lock = "";
+                globalConn.send({
+                  type: "lock",
+                  payload: "",
+                });
+              }, 1000);
+              globalConn?.send({
+                type: "draw",
+                payload: {
+                  data: canvas.current.getSaveData(),
+                  lock: {
+                    lock,
+                  },
+                },
+              });
+            }
+          }}
+        />
       </div>
-      <div className="videos" style={{ display: isConnecting ? "" : "none" }}>
+      <Layout className="layout" style={{ minHeight: "100vh" }}>
+        <Header>
+          <div style={{ color: "white", fontSize: "20px" }}>
+            <Space size="middle">
+              <VideoCameraTwoTone />
+              在线授课系统
+              <Button
+                onClick={() => document.documentElement.requestFullscreen()}
+              >
+                全屏
+              </Button>
+            </Space>
+          </div>
+        </Header>
         <div
+          className="panel"
           style={{
-            display: "flex",
+            display: !isConnecting ? "flex" : "none",
             justifyContent: "center",
             alignItems: "center",
             height: "80vh",
           }}
         >
-          <div style={{ margin: "32px" }}>
-            <video style={{ width: "100%", height: "100%" }} id="video1" />
+          <Card style={{ width: "500px", height: "300px", margin: "32px" }}>
+            <Space direction="vertical">
+              <Title level={4}>基本信息</Title>
+              <div style={{ fontSize: "16px" }}>
+                个人ID：<Text type="secondary">{key}</Text>
+              </div>
+              <div style={{ fontSize: "16px" }}>
+                在线时长：<Text type="secondary">{"<10分钟"}</Text>
+              </div>
+            </Space>
+          </Card>
+          <Card style={{ width: "500px", height: "300px" }}>
+            <Space direction="vertical" style={{ width: "100%" }}>
+              <Title level={4}>发起聊天</Title>
+              <div>
+                <Input.Search
+                  onSearch={(value) => setInputValue(value)}
+                  placeholder="请输入对方的ID"
+                />
+              </div>
+            </Space>
+          </Card>
+        </div>
+        <div className="videos" style={{ display: isConnecting ? "" : "none" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "80vh",
+            }}
+          >
+            <div style={{ margin: "32px" }}>
+              <video style={{ width: "100%", height: "100%" }} id="video1" />
+            </div>
+            <div style={{ margin: "32px" }}>
+              <video style={{ width: "100%", height: "100%" }} id="video2" />
+            </div>
           </div>
-          <div style={{ margin: "32px" }}>
-            <video style={{ width: "100%", height: "100%" }} id="video2" />
+          <div
+            style={{
+              zIndex: 100,
+              position: "fixed",
+              right: "0",
+              bottom: "100px",
+              padding: "16px",
+            }}
+          >
+            <Space style={{ width: "100%" }} direction="vertical" size="large">
+              <div
+                onClick={() => {
+                  // setInputValue(undefined);
+                  window.location.reload();
+                }}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "24px",
+                    color: "white",
+                    width: "48px",
+                    height: "48px",
+                    background: "#1890ff",
+                    borderRadius: "100%",
+                    marginRight: "16px",
+                  }}
+                >
+                  <PhoneOutlined />
+                </div>
+                挂断通话
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "24px",
+                    color: "white",
+                    width: "48px",
+                    height: "48px",
+                    background: "#1890ff",
+                    borderRadius: "100%",
+                    marginRight: "16px",
+                  }}
+                  onClick={() => {
+                    setWhiteBoard((value) => {
+                      globalConn.send({ type: "white", payload: !value });
+                      return !value;
+                    });
+                  }}
+                >
+                  <FormOutlined />
+                </div>
+                开启白板
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "24px",
+                    color: "white",
+                    width: "48px",
+                    height: "48px",
+                    background: "#1890ff",
+                    borderRadius: "100%",
+                    marginRight: "16px",
+                  }}
+                >
+                  <FileOutlined />
+                </div>
+                发送文件
+              </div>
+            </Space>
           </div>
         </div>
-        <div
+        <Footer
           style={{
+            width: "100%",
+            textAlign: "center",
             position: "fixed",
-            right: "0",
-            bottom: "100px",
-            padding: "16px",
+            left: "0",
+            bottom: "0",
+            background: "white",
           }}
         >
-          <Space style={{ width: "100%" }} direction="vertical" size="large">
-            <div
-              onClick={() => {
-                myPeer.destroy();
-                myPeer = undefined;
-                setIsConnecting(false);
-              }}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  fontSize: "24px",
-                  color: "white",
-                  width: "48px",
-                  height: "48px",
-                  background: "#1890ff",
-                  borderRadius: "100%",
-                  marginRight: "16px",
-                }}
-              >
-                <PhoneOutlined />
-              </div>
-              挂断通话
-            </div>
-
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  fontSize: "24px",
-                  color: "white",
-                  width: "48px",
-                  height: "48px",
-                  background: "#1890ff",
-                  borderRadius: "100%",
-                  marginRight: "16px",
-                }}
-              >
-                <FormOutlined />
-              </div>
-              开启白板
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  fontSize: "24px",
-                  color: "white",
-                  width: "48px",
-                  height: "48px",
-                  background: "#1890ff",
-                  borderRadius: "100%",
-                  marginRight: "16px",
-                }}
-              >
-                <FileOutlined />
-              </div>
-              发送文件
-            </div>
-          </Space>
-        </div>
-      </div>
-      <Footer
-        style={{
-          width: "100%",
-          textAlign: "center",
-          position: "fixed",
-          left: "0",
-          bottom: "0",
-          background: "white",
-        }}
-      >
-        {" "}
-        ©2021 Designed by Rui
-      </Footer>
-    </Layout>
+          {" "}
+          ©2021 Designed by Rui
+        </Footer>
+      </Layout>
+    </>
   );
 }
